8ddb9aa36e11266dfa412dcf8918dfbe42be4080
emonmorol <ekramulhasane69@gmail.com>
2022-06-04 03:59:41 +0600

    package set up

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hi , This is emon morol");
