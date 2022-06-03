906caa569c3e54f06ea36395ce0cf83a0efcf623
emonmorol <ekramulhasane69@gmail.com>
2022-06-04 04:36:40 +0600

    email send successfully

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const nodemailer = require("nodemailer");
 const mg = require("nodemailer-mailgun-transport");
 
 app.use(cors());
 app.use(express.json());
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+};
+
+const sendEmail = nodemailer.createTransport(mg(auth));
+
+app.post("/email", async (req, res) => {
+  const { name, email, phone, subject, message } = req.body;
+
+  sendEmail.sendMail(
+    {
+      from: email,
+      to: "ekramulhasanmorol@gmail.com",
+      subject: subject,
+      text: message,
+    },
+    (err, info) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(info);
+      }
+    }
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Hi , This is emon morol");
